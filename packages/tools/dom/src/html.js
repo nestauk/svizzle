1e52d2dde0106ab4f033b5f1574184b1e7494db7
Luca Bonavita <mindrones@users.noreply.github.com>
2020-01-15 16:32:06 +0000

    /dom: document `alignTags`

--- packages/tools/dom/src/html.js
+++ packages/tools/dom/src/html.js
@@ -2,6 +2,38 @@
 * @module @svizzle/dom/html
 */
 
+/**
+ * Return a copy of the input HTML string formatted by inserting return chars between tags.
+ *
+ * @function
+ * @arg {string} - HTML string
+ * @return {string} - formatted HTML string
+ *
+ * @example
+> const input = '<main><div class="item"><div class="labels"><span>foo</span></div> </div></main>'
+> alignTags(input)
+<main>
+<div class="item">
+<div class="labels">
+<span>foo</span>
+</div>
+</div>
+</main>
+
+> const input = '<main><div class="item"><div class="labels"><span>[Object object]</span></div> </div></main>'
+> alignTags(input)
+<main>
+<div class="item">
+<div class="labels">
+<span>
+[Object object]
+</span>
+</div>
+</div>
+</main>
+ *
+ * @version 0.3.0
+ */
 export const alignTags = htmlString =>
   htmlString
   .replace(/>\s*</gu, '>\n<')
