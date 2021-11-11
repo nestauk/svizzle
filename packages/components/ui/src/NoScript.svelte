bb507a4522777d784a50400e60823ac3c2245542
Sebastian Ferreyra <ushiferreyra@gmail.com>
2021-11-11 16:33:18 -0300

    Added `NoScript` component (#372)
    Closes #358

---
+++ packages/components/ui/src/NoScript.svelte
@@ -0,0 +1,34 @@
+<noscript>
+	<div>
+		<p>
+			Please enable Javascript in your browser and reload.
+		</p>
+	</div>
+</noscript>
+
+<style>
+	noscript {
+		background-color: #CCC;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		justify-content: center;
+		margin: auto;
+		position: absolute;
+		width: 100%;
+		z-index: var(--z-2000);
+	}
+
+	div {
+		background-color: white;
+		border-radius: 1rem;
+		margin: auto;
+		padding: 2rem;
+		position: relative;
+		width: 90%;
+	}
+
+	p {
+		margin: 0.75rem 0;
+	}
+</style>
