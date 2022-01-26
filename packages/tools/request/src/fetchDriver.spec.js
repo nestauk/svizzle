3fd8e37edf7b3a72f16a40a1031260e7eff2cf75
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2022-01-26 11:09:14 -0300

    Refactored code, added dynamic ports and now closes server on fail

--- packages/tools/request/src/fetchDriver.spec.js
+++ packages/tools/request/src/fetchDriver.spec.js
@@ -4,69 +4,30 @@ import {fetch} from 'undici'
 import http from 'http'
 import serveHandler from 'serve-handler'
 import path from 'path'
-import {createReadStream} from 'fs'
+import {createReadStream, readdirSync} from 'fs'
 import Throttle from 'throttle'
 import {readJson} from '@svizzle/file'
 
 import {makeFetchDriver} from './fetchDriver.js'
 
+// for debugging
 const DEBUG = false
 const debug = (...args) => DEBUG && console.log(...args)
 
-const basePath = path.resolve('../atlas/distro')
+// test environment
+const baseServerPath = path.resolve('../atlas/distro')
 
-/*
-const sources = Object.fromEntries(
-	fs.readdirSync(basePath)
-	.filter(fn => fn.endsWith('.json'))
-	.filter(fn => /.*\d\.json$/.test(fn))
-	.map(fn => [
-		`NUTS-${fn.substr(12,4)}-${fn.substr(27,1)}-${fn.substr(8,2)}`,
-		{url:'http://localhost:4000/' + fn}
-	]))
-*/
-const sources = {
-	'NUTS-2003-0-03': { url: 'http://localhost:4000/NUTS_RG_03M_2003_4326_LEVL_0.json' },
-	'NUTS-2003-1-03': { url: 'http://localhost:4000/NUTS_RG_03M_2003_4326_LEVL_1.json' },
-	'NUTS-2003-2-03': { url: 'http://localhost:4000/NUTS_RG_03M_2003_4326_LEVL_2.json' },
-	'NUTS-2003-3-03': { url: 'http://localhost:4000/NUTS_RG_03M_2003_4326_LEVL_3.json' },
-	'NUTS-2006-0-03': { url: 'http://localhost:4000/NUTS_RG_03M_2006_4326_LEVL_0.json' },
-	'NUTS-2006-1-03': { url: 'http://localhost:4000/NUTS_RG_03M_2006_4326_LEVL_1.json' },
-	'NUTS-2006-2-03': { url: 'http://localhost:4000/NUTS_RG_03M_2006_4326_LEVL_2.json' },
-	'NUTS-2006-3-03': { url: 'http://localhost:4000/NUTS_RG_03M_2006_4326_LEVL_3.json' },
-	'NUTS-2010-0-03': { url: 'http://localhost:4000/NUTS_RG_03M_2010_4326_LEVL_0.json' },
-	'NUTS-2010-1-03': { url: 'http://localhost:4000/NUTS_RG_03M_2010_4326_LEVL_1.json' },
-	'NUTS-2010-2-03': { url: 'http://localhost:4000/NUTS_RG_03M_2010_4326_LEVL_2.json' },
-	'NUTS-2010-3-03': { url: 'http://localhost:4000/NUTS_RG_03M_2010_4326_LEVL_3.json' },
-	'NUTS-2013-0-03': { url: 'http://localhost:4000/NUTS_RG_03M_2013_4326_LEVL_0.json' },
-	'NUTS-2013-1-03': { url: 'http://localhost:4000/NUTS_RG_03M_2013_4326_LEVL_1.json' },
-	'NUTS-2013-2-03': { url: 'http://localhost:4000/NUTS_RG_03M_2013_4326_LEVL_2.json' },
-	'NUTS-2013-3-03': { url: 'http://localhost:4000/NUTS_RG_03M_2013_4326_LEVL_3.json' },
-	'NUTS-2016-0-03': { url: 'http://localhost:4000/NUTS_RG_03M_2016_4326_LEVL_0.json' },
-	'NUTS-2016-1-03': { url: 'http://localhost:4000/NUTS_RG_03M_2016_4326_LEVL_1.json' },
-	'NUTS-2016-2-03': { url: 'http://localhost:4000/NUTS_RG_03M_2016_4326_LEVL_2.json' },
-	'NUTS-2016-3-03': { url: 'http://localhost:4000/NUTS_RG_03M_2016_4326_LEVL_3.json' },
-	'NUTS-2003-0-10': { url: 'http://localhost:4000/NUTS_RG_10M_2003_4326_LEVL_0.json' },
-	'NUTS-2003-1-10': { url: 'http://localhost:4000/NUTS_RG_10M_2003_4326_LEVL_1.json' },
-	'NUTS-2003-2-10': { url: 'http://localhost:4000/NUTS_RG_10M_2003_4326_LEVL_2.json' },
-	'NUTS-2003-3-10': { url: 'http://localhost:4000/NUTS_RG_10M_2003_4326_LEVL_3.json' },
-	'NUTS-2006-0-10': { url: 'http://localhost:4000/NUTS_RG_10M_2006_4326_LEVL_0.json' },
-	'NUTS-2006-1-10': { url: 'http://localhost:4000/NUTS_RG_10M_2006_4326_LEVL_1.json' },
-	'NUTS-2006-2-10': { url: 'http://localhost:4000/NUTS_RG_10M_2006_4326_LEVL_2.json' },
-	'NUTS-2006-3-10': { url: 'http://localhost:4000/NUTS_RG_10M_2006_4326_LEVL_3.json' },
-	'NUTS-2010-0-10': { url: 'http://localhost:4000/NUTS_RG_10M_2010_4326_LEVL_0.json' },
-	'NUTS-2010-1-10': { url: 'http://localhost:4000/NUTS_RG_10M_2010_4326_LEVL_1.json' },
-	'NUTS-2010-2-10': { url: 'http://localhost:4000/NUTS_RG_10M_2010_4326_LEVL_2.json' },
-	'NUTS-2010-3-10': { url: 'http://localhost:4000/NUTS_RG_10M_2010_4326_LEVL_3.json' },
-	'NUTS-2013-0-10': { url: 'http://localhost:4000/NUTS_RG_10M_2013_4326_LEVL_0.json' },
-	'NUTS-2013-1-10': { url: 'http://localhost:4000/NUTS_RG_10M_2013_4326_LEVL_1.json' },
-	'NUTS-2013-2-10': { url: 'http://localhost:4000/NUTS_RG_10M_2013_4326_LEVL_2.json' },
-	'NUTS-2013-3-10': { url: 'http://localhost:4000/NUTS_RG_10M_2013_4326_LEVL_3.json' },
-	'NUTS-2016-0-10': { url: 'http://localhost:4000/NUTS_RG_10M_2016_4326_LEVL_0.json' },
-	'NUTS-2016-1-10': { url: 'http://localhost:4000/NUTS_RG_10M_2016_4326_LEVL_1.json' },
-	'NUTS-2016-2-10': { url: 'http://localhost:4000/NUTS_RG_10M_2016_4326_LEVL_2.json' },
-	'NUTS-2016-3-10': { url: 'http://localhost:4000/NUTS_RG_10M_2016_4326_LEVL_3.json' }
-}
+const fileNamesMap = _.pipe([
+	_.filterWith(fileName => fileName.endsWith('.json')),
+	_.filterWith(fileName => /.*\d\.json$/.test(fileName)),
+	_.mapWith(fileName => [
+		`NUTS-${fileName.substr(12,4)}-${fileName.substr(27,1)}-${fileName.substr(8,2)}`,
+		fileName
+	]),
+	_.fromPairs
+// eslint-disable-next-line no-sync
+])(readdirSync(baseServerPath))
+
 
 const decoder = new TextDecoder()
 const decode = bytes => decoder.decode(bytes)
@@ -75,7 +36,19 @@ const jsonParser = _.pipe([
 	JSON.parse
 ])
 
-const TIMEOUT = 40000
+const TIMEOUT = 20000
+
+let serverPort = 4000
+
+// utils
+const makeSources = baseUri => _.pipe([
+	_.pairs,
+	_.mapWith(([key, fileName]) => [
+		key,
+		{url: baseUri + fileName}
+	]),
+	_.fromPairs
+])
 
 const startServer = ({bps, port, basePath}) => {
 	let middleware
@@ -100,30 +73,24 @@ const startServer = ({bps, port, basePath}) => {
 	return server
 }
 
-const loadJsons = async keys => {
+const loadJsons = async (keys, filesMap) => {
 	const fileLoadingPromises = _.pipe([
 		_.pairs,
 		_.filterWith(([key]) => keys.includes(key)),
-		_.mapWith(([key, spec]) =>
-			[key, basePath + spec.url.replace('http://localhost:4000','')]
+		_.mapWith(([key, fileName]) =>
+			[key, `${baseServerPath}/${fileName}`]
 		),
 		_.mapWith(async ([key, filePath]) =>
 			[key, await readJson(filePath)])
-	])(sources)
+	])(filesMap)
 
 	const loadedFiles = await Promise.all(fileLoadingPromises)
 	return _.fromPairs(loadedFiles)
 }
 
 describe('fetchDriver', function () {
-	it('should only load files in asapKeys', function () {
-		this.timeout(TIMEOUT)
-
-		const server = startServer({
-			bps: 1024 * 512,
-			port: 4000,
-			basePath
-		})
+	describe('shouldPrefecth = false', function () {
+		const sources = makeSources(`http://localhost:${serverPort}/`)(fileNamesMap)
 
 		const priorities = {
 			asap: [
@@ -136,113 +103,120 @@ describe('fetchDriver', function () {
 				'NUTS-2016-2-03',
 			]
 		}
-
 		const sourcesCount = priorities.asap.length
 		debug('sourcesCount', sourcesCount)
-		const {
-			_asapKeys,
-			_defaultTransformer,
-			_nextKeys,
-			_outData,
-			_outLoadingKeys,
-			_shouldPrefetch,
-			_uriMap
-		} = makeFetchDriver(fetch)
 
-		_shouldPrefetch.next(false)
-		_defaultTransformer.next(jsonParser)
-		_uriMap.next(sources)
-		_asapKeys.next(priorities.asap)
-		_nextKeys.next(priorities.next)
+		const server = startServer({
+			// bps: 1024 * 512,
+			port: serverPort++,
+			basePath: baseServerPath
+		})
+		after(function () {
+			server.close()
+		})
+		it('should only load files in asapKeys', function () {
+			this.timeout(TIMEOUT)
 
-		return new Promise(resolve => {
-			DEBUG && _outLoadingKeys.subscribe(loadingKeys => {
-				debug('loadingKeys', loadingKeys.length)
-			})
+			const {
+				_asapKeys,
+				_defaultTransformer,
+				_nextKeys,
+				_outData,
+				_outLoadingKeys,
+				_shouldPrefetch,
+				_uriMap
+			} = makeFetchDriver(fetch)
 
-			_outData.subscribe(async data => {
-				const keys = _.keys(data)
-				debug('loaded', keys.length)
-				if (keys.length === sourcesCount) {
-					server.close()
-					try {
-						const expectedJsons = await loadJsons(keys)
-						assert.deepStrictEqual(data, expectedJsons)
-					} catch (e) {
-						console.error(e)
-					} finally {
+			_shouldPrefetch.next(false)
+			_defaultTransformer.next(jsonParser)
+			_uriMap.next(sources)
+			_asapKeys.next(priorities.asap)
+			_nextKeys.next(priorities.next)
+
+			return new Promise((resolve, reject) => {
+				DEBUG && _outLoadingKeys.subscribe(loadingKeys => {
+					debug('loadingKeys', loadingKeys.length)
+				})
+
+				_outData.subscribe(async data => {
+					const keys = _.keys(data)
+					debug('loaded', keys.length)
+					if (keys.length === sourcesCount) {
+						try {
+							const expectedJsons = await loadJsons(keys, fileNamesMap)
+							assert.deepStrictEqual(data, expectedJsons)
+						} catch (e) {
+							reject(e)
+						}
 						resolve()
 					}
-				}
+				})
 			})
 		})
 	})
 
-	it('should load all files in sources', function () {
-		this.timeout(TIMEOUT)
-
-		const server = startServer({
-			// bps: 1024 * 512,
-			port: 4000,
-			basePath
-		})
+	describe('asapKeys = keys(sources)', function () {
+		const sources = makeSources(`http://localhost:${serverPort}/`)(fileNamesMap)
 
 		const priorities = {
 			asap: [],
 			next: []
 		}
 		priorities.asap = _.keys(sources)
-
 		const sourcesCount = priorities.asap.length
-
 		debug('sourcesCount', sourcesCount)
-		const {
-			_asapKeys,
-			_defaultTransformer,
-			_nextKeys,
-			_outData,
-			_outLoadingKeys,
-			_shouldPrefetch,
-			_uriMap
-		} = makeFetchDriver(fetch)
 
-		_shouldPrefetch.next(false)
-		_defaultTransformer.next(jsonParser)
-		_uriMap.next(sources)
-		_asapKeys.next(priorities.asap)
-		_nextKeys.next(priorities.next)
+		const server = startServer({
+			// bps: 1024 * 512,
+			port: serverPort++,
+			basePath: baseServerPath
+		})
+		after(function () {
+			server.close()
+		})
+		it('should load all files in sources', function () {
+			this.timeout(TIMEOUT)
 
-		return new Promise(resolve => {
-			DEBUG && _outLoadingKeys.subscribe(loadingKeys => {
-				debug('loadingKeys', loadingKeys.length)
-			})
+			const {
+				_asapKeys,
+				_defaultTransformer,
+				_nextKeys,
+				_outData,
+				_outLoadingKeys,
+				_shouldPrefetch,
+				_uriMap
+			} = makeFetchDriver(fetch)
 
-			_outData.subscribe(async data => {
-				const keys = _.keys(data)
-				debug('loaded', keys.length)
-				if (keys.length === sourcesCount) {
-					server.close()
-					try {
-						const expectedJsons = await loadJsons(keys)
-						assert.deepStrictEqual(data, expectedJsons)
-					} catch (e) {
-						console.error(e)
-					} finally {
+			_shouldPrefetch.next(false)
+			_defaultTransformer.next(jsonParser)
+			_uriMap.next(sources)
+			_asapKeys.next(priorities.asap)
+			_nextKeys.next(priorities.next)
+
+			return new Promise((resolve, reject) => {
+				DEBUG && _outLoadingKeys.subscribe(loadingKeys => {
+					debug('loadingKeys', loadingKeys.length)
+				})
+
+				_outData.subscribe(async data => {
+					const keys = _.keys(data)
+					debug('loaded', keys.length)
+					if (keys.length === sourcesCount) {
+						try {
+							const expectedJsons = await loadJsons(keys, fileNamesMap)
+							assert.deepStrictEqual(data, expectedJsons)
+						} catch (e) {
+							reject(e)
+						}
 						resolve()
 					}
-				}
+				})
 			})
 		})
 	})
 
-	it('should load all files in correct order', function () {
-		this.timeout(TIMEOUT)
-
-		const server = startServer({
-			bps: 1024 * 512,
-			port: 4000,
-			basePath
-		})
+	describe('load order', function () {
+		const sources = makeSources(`http://localhost:${serverPort}/`)(fileNamesMap)
 
 		const priorities = {
 			asap: [
@@ -255,76 +229,86 @@ describe('fetchDriver', function () {
 				'NUTS-2016-2-03',
 			]
 		}
-
 		const sourcesCount = _.keys(sources).length
 		debug('sourcesCount', sourcesCount)
-		const {
-			_asapKeys,
-			_defaultTransformer,
-			_nextKeys,
-			_outData,
-			_outLoadingKeys,
-			_shouldPrefetch,
-			_uriMap
-		} = makeFetchDriver(fetch)
 
-		_shouldPrefetch.next(true)
-		_defaultTransformer.next(jsonParser)
-		_uriMap.next(sources)
-		_asapKeys.next(priorities.asap)
-		_nextKeys.next(priorities.next)
+		const server = startServer({
+			// bps: 1024 * 512,
+			port: serverPort++,
+			basePath: baseServerPath
+		})
+		after(function () {
+			server.close()
+		})
+		it('should load all files in correct order', function () {
+			this.timeout(TIMEOUT)
+			const {
+				_asapKeys,
+				_defaultTransformer,
+				_nextKeys,
+				_outData,
+				_outLoadingKeys,
+				_shouldPrefetch,
+				_uriMap
+			} = makeFetchDriver(fetch)
 
-		return new Promise(resolve => {
-			DEBUG && _outLoadingKeys.subscribe(loadingKeys => {
-				debug('loadingKeys', loadingKeys.length)
-			})
+			_shouldPrefetch.next(true)
+			_defaultTransformer.next(jsonParser)
+			_uriMap.next(sources)
+			_asapKeys.next(priorities.asap)
+			_nextKeys.next(priorities.next)
 
-			let orderedKeys = []
+			return new Promise(resolve => {
+				DEBUG && _outLoadingKeys.subscribe(loadingKeys => {
+					debug('loadingKeys', loadingKeys.length)
+				})
 
-			_outData.subscribe(data => {
-				const keys = _.keys(data)
-				orderedKeys = [
-					...orderedKeys,
-					..._.difference(keys, orderedKeys)
-				]
-				debug('loaded', keys.length)
-				if (keys.length === sourcesCount) {
-					server.close()
-					const actualAsap = orderedKeys.slice(
-						0,
-						priorities.asap.length
-					)
-					const actualNext = orderedKeys.slice(
-						priorities.asap.length,
-						priorities.asap.length + priorities.next.length
-					)
-					const actualRest = orderedKeys.slice(
-						priorities.asap.length + priorities.next.length
-					)
-					debug('actualAsap', actualAsap)
-					debug('actualNext', actualNext)
-					debug('actualRest', actualRest)
+				let orderedKeys = []
 
-					assert.deepStrictEqual(
-						actualAsap.sort(),
-						priorities.asap.sort(),
-						'Asap not equal'
-					)
-					assert.deepStrictEqual(
-						actualNext.sort(),
-						priorities.next.sort(),
-						'Next not equal'
-					)
-					assert.deepStrictEqual(
-						actualRest.sort(),
-						_.difference(
-							keys,
-							_.union(priorities.asap, priorities.next)
-						).sort(),
-						'Rest not equal'
-					)
-					resolve()
-				}
+				_outData.subscribe(data => {
+					const keys = _.keys(data)
+					orderedKeys = [
+						...orderedKeys,
+						..._.difference(keys, orderedKeys)
+					]
+					debug('loaded', keys.length)
+					if (keys.length === sourcesCount) {
+						const actualAsap = orderedKeys.slice(
+							0,
+							priorities.asap.length
+						)
+						const actualNext = orderedKeys.slice(
+							priorities.asap.length,
+							priorities.asap.length + priorities.next.length
+						)
+						const actualRest = orderedKeys.slice(
+							priorities.asap.length + priorities.next.length
+						)
+						debug('actualAsap', actualAsap)
+						debug('actualNext', actualNext)
+						debug('actualRest', actualRest)
+
+						assert.deepStrictEqual(
+							actualAsap.sort(),
+							priorities.asap.sort(),
+							'Asap not equal'
+						)
+						assert.deepStrictEqual(
+							actualNext.sort(),
+							priorities.next.sort(),
+							'Next not equal'
+						)
+						assert.deepStrictEqual(
+							actualRest.sort(),
+							_.difference(
+								keys,
+								_.union(priorities.asap, priorities.next)
+							).sort(),
+							'Rest not equal'
+						)
+						resolve()
+					}
+				})
 			})
 		})
 	})
