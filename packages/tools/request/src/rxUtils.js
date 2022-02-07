ab25ca2e57f0c64f82b2b7c21c3f104eb4b76b7f
Luca Bonavita <mindrones@users.noreply.github.com>
2022-02-07 12:29:22 +0000

    formatting + moved effectors in the sfx section

--- packages/tools/request/src/rxUtils.js
+++ packages/tools/request/src/rxUtils.js
@@ -1,12 +1,13 @@
 import {combineLatest} from 'rxjs';
 import {map, debounceTime, share} from 'rxjs/operators';
 
-export const derive = (observables, fn) => combineLatest(...observables)
-.pipe(
-	debounceTime(0),
-	map(fn),
-	share()
-);
+export const derive = (observables, fn) =>
+	combineLatest(...observables)
+	.pipe(
+		debounceTime(0),
+		map(fn),
+		share()
+	);
 
 /*
 const derive2 = (main, observables, fn) => main
