35cbde828524460fd971911f923146fe6676a9e1
Luca Bonavita <mindrones@users.noreply.github.com>
2022-02-10 19:21:59 +0000

    fomatting /request/src/fetchManager/utils.js

--- packages/tools/request/src/fetchManager/utils.js
+++ packages/tools/request/src/fetchManager/utils.js
@@ -1,17 +1,17 @@
-import {createReadStream} from 'fs'
-import http from 'http'
+import {createReadStream} from 'fs';
+import http from 'http';
 
-import {readJson} from '@svizzle/file'
-import * as _ from 'lamb'
-import serveHandler from 'serve-handler'
-import Throttle from 'throttle'
+import {readJson} from '@svizzle/file';
+import * as _ from 'lamb';
+import serveHandler from 'serve-handler';
+import Throttle from 'throttle';
 
-const decoder = new TextDecoder()
-const decode = bytes => decoder.decode(bytes)
+const decoder = new TextDecoder();
+const decode = bytes => decoder.decode(bytes);
 export const jsonParser = _.pipe([
 	decode,
 	JSON.parse
-])
+]);
 
 export const loadJsons = async (basePath, keys, filesMap) => {
 	const fileLoadingPromises = _.pipe([
@@ -22,10 +22,10 @@ export const loadJsons = async (basePath, keys, filesMap) => {
 		),
 		_.mapWith(async ([key, filePath]) =>
 			[key, await readJson(filePath)])
-	])(filesMap)
+	])(filesMap);
 
-	const loadedFiles = await Promise.all(fileLoadingPromises)
-	return _.fromPairs(loadedFiles)
+	const loadedFiles = await Promise.all(fileLoadingPromises);
+	return _.fromPairs(loadedFiles);
 }
 
 export const getFileNamesMap = _.pipe([
@@ -36,8 +36,7 @@ export const getFileNamesMap = _.pipe([
 		fileName
 	]),
 	_.fromPairs
-// eslint-disable-next-line no-sync
-])
+]);
 
 export const makeUriMap = baseUri => _.pipe([
 	_.pairs,
@@ -46,31 +45,33 @@ export const makeUriMap = baseUri => _.pipe([
 		{url: baseUri + fileName}
 	]),
 	_.fromPairs
-])
+]);
 
 export const getKeysNamed = string =>
-	_.filterWith(key => key.includes(string))
+	_.filterWith(key => key.includes(string));
 
 export const startServer = ({bandwidth, port, basePath}) => {
-	let middleware
+	let middleware;
+
 	if (bandwidth) {
 		middleware = {
 			createReadStream (filePath, config) {
-				const throttledStream = new Throttle({bps: bandwidth})
-				createReadStream(filePath, config).pipe(throttledStream)
-				return throttledStream
+				const throttledStream = new Throttle({bps: bandwidth});
+				createReadStream(filePath, config).pipe(throttledStream);
+				return throttledStream;
 			}
 		}
 	}
+
 	const server = http.createServer(async (req, res) => {
 		await serveHandler(
 			req,
 			res,
 			{public: basePath},
 			middleware
-		)
+		);
 	})
-	server.listen(port)
-	return server
-}
+	server.listen(port);
 
+	return server;
+}
